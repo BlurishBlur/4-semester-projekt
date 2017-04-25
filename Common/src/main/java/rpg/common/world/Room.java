29030fb7a51cb26469508dead54933aab90a2dd0
Niels <Niels@DESKTOP-4QGMF1S>
2017-04-25 14:31:26 +0200

    blocked

--- Common/src/main/java/rpg/common/world/Room.java
+++ Common/src/main/java/rpg/common/world/Room.java
@@ -9,9 +9,10 @@ import java.util.Stack;
 import java.util.concurrent.ConcurrentHashMap;
 import rpg.common.entities.Entity;
 import rpg.common.util.Polygon;
+import rpg.common.util.Vector;
 
 public class Room implements Serializable {
-    
+
     private final Map<String, Entity> entities = new ConcurrentHashMap<>();
     private int width = 1280;
     private int height = 720;
@@ -23,13 +24,44 @@ public class Room implements Serializable {
     private boolean canExitRight;
     private String spritePath;
     private Stack<Polygon> collidables;
-    
+
     public Room(String spritePath) {
         this.spritePath = spritePath;
     }
-    
+
     public Room() {
         collidables = new Stack();
+    }
+
+    public boolean blocked(Entity entity, float targetX, float targetY) {
+        return isPointInPolygons(collidables,
+                new Vector(entity.getRoomPosition().getX() + targetX, entity.getRoomPosition().getY() + targetY));
+    }
+
+    private boolean isPointInPolygons(Stack<Polygon> polygons, Vector point) {
+        if (polygons != null) {
+            for (Polygon polygon : polygons) {
+                if (isPointInPolygon(polygon, point)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isPointInPolygon(Polygon polygon, Vector point) {
+        Vector lastVertice = polygon.getLast();
+        boolean oddNodes = false;
+        for (int i = 0; i < polygon.size(); i++) {
+            Vector vertice = polygon.get(i);
+            if ((vertice.getY() < point.getY() && lastVertice.getY() >= point.getY()) || (lastVertice.getY() < point.getY() && vertice.getY() >= point.getY())) {
+                if (vertice.getX() + (point.getY() - vertice.getY()) / (lastVertice.getY() - vertice.getY()) * (lastVertice.getX() - vertice.getX()) < point.getX()) {
+                    oddNodes = !oddNodes;
+                }
+            }
+            lastVertice = vertice;
+        }
+        return oddNodes;
     }
 
     public int getX() {
@@ -47,47 +79,47 @@ public class Room implements Serializable {
     public void setY(int y) {
         this.y = y;
     }
-    
+
     public Stack<Polygon> getCollidables() {
         return collidables;
     }
-    
+
     public void setCollidables(Stack<Polygon> collidables) {
         this.collidables = collidables;
     }
-    
+
     public void canExitUp(boolean canExitUp) {
         this.canExitUp = canExitUp;
     }
-    
+
     public void canExitDown(boolean canExitDown) {
         this.canExitDown = canExitDown;
     }
-    
+
     public void canExitLeft(boolean canExitLeft) {
         this.canExitLeft = canExitLeft;
     }
-    
+
     public void canExitRight(boolean canExitRight) {
         this.canExitRight = canExitRight;
     }
-    
+
     public boolean canExitUp() {
         return canExitUp;
     }
-    
+
     public boolean canExitDown() {
         return canExitDown;
     }
-    
+
     public boolean canExitLeft() {
         return canExitLeft;
     }
-    
+
     public boolean canExitRight() {
         return canExitRight;
     }
-    
+
     public void addEntity(Entity entity) {
         entities.put(entity.getID(), entity);
     }
@@ -143,7 +175,5 @@ public class Room implements Serializable {
     public void setSpritePath(String spritePath) {
         this.spritePath = spritePath;
     }
-    
-    
-    
+
 }
